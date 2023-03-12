f44763ccaa44065d3c4bacefd1b61e3e3f31aab7
cabdiaziz <abdiaziiz1856@gmail.com>
2023-03-12 11:31:03 +0300

    added customized tailwind css and designed index.js page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,36 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1 className="text-green-600">Home Page</h1>;
+  return (
+    <div>
+      {/* nav bar */}
+      <div className="bg-blue-400 border-4 border-green-700">
+        <h1 className="text-xs sm:text-base md:text-lg lg:text-3xl font-bold underline text-left">
+          Portfolio
+        </h1>
+        <ul className="text-right">
+          <li>home</li>
+          <li>about</li>
+          <li>contact</li>
+        </ul>
+      </div>
+
+      {/* image as background with my picture */}
+      <Image
+        src=""
+        alt="abdiaziz"
+        width="1750"
+        height="100"
+        className="border-4 border-yellow-200"
+      />
+
+      {/* personal info */}
+      <div className="bg-sky-200 border-4 border-sky-400 italic">
+        <p className="decoration-1">Name : abdiaziz abdullahi</p>
+        <p className="">Email : naadirinho@gmail.com</p>
+        <p className="">Phone : 252619765666</p>
+        <p className="">Location : mogadishu-somalia</p>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        next: {
+          50: "#fe19fa", //allow us to override only when we use it inside extend object.
+          //* you can give 10: value, 100: value , etc..
+        },
+      },
+    },
   },
   plugins: [],
 };
